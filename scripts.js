14ae8f4b654bb1d13ac7719727fa249351382a5e
Aleena <amehm5@uic.edu>
2025-01-30 15:10:11 -0600

    starter code

---
+++ scripts.js
@@ -0,0 +1,19 @@
+document.getElementById('about-btn').addEventListener('click', () => {
+    document.getElementById('about').scrollIntoView({ behavior: 'smooth' });
+});
+
+document.getElementById('schedule-btn').addEventListener('click', () => {
+    document.getElementById('schedule').scrollIntoView({ behavior: 'smooth' });
+});
+
+document.getElementById('faq-btn').addEventListener('click', () => {
+    document.getElementById('faq').scrollIntoView({ behavior: 'smooth' });
+});
+
+document.getElementById('sponsors-btn').addEventListener('click', () => {
+    document.getElementById('sponsors').scrollIntoView({ behavior: 'smooth' });
+});
+
+document.getElementById('resources-btn').addEventListener('click', () => {
+    document.getElementById('resources').scrollIntoView({ behavior: 'smooth' });
+});
